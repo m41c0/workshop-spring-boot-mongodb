aabf5e62358e667693016442f6215fb1a22661cc
Maicon Ferreira <m41c0n@outlook.com>
2021-09-06 00:57:34 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/m41c0n/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.m41c0n.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.m41c0n.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+}
