9b81de4416f2df47842ca0593436b561fc3edf23
Maicon Ferreira <m41c0n@outlook.com>
2021-09-06 03:13:48 -0300

    Consulta com vários critérios

--- src/main/java/com/m41c0n/workshopmongo/services/PostService.java
+++ src/main/java/com/m41c0n/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.m41c0n.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,5 +28,9 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime()+ (24 * 60 * 60 * 1000));
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 	
 }
